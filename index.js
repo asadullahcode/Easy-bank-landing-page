9120ad089270e48ba5ae6d4e40ffffbcebd6f2c6
asadullahcode <kingasadullah9@gmail.com>
2024-08-14 18:24:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function toggleHide(){
+     let firstpara= document.getElementsByClassName('firstpara');
+     let btn= document.getElementsByClassName('btn1');
+
+     firstpara.style.display =`none`;
+}
